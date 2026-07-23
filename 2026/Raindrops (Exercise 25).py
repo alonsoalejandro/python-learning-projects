ad7b73ef0be0fb4e584df876b151161463d2dd4c
Alejandro Alonso Iglesias <alejandro.aloigl@gmail.com>
2026-07-24 00:39:27 +0200

    Create Raindrops (Exercise 25).py

---
+++ 2026/Raindrops (Exercise 25).py
@@ -0,0 +1,15 @@
+def convert(number):
+    answer = ""
+    if number % 3 == 0:
+        answer += "Pling"
+    
+    if number % 5 == 0:
+        answer += "Plang"
+    
+    if number % 7 == 0:
+        answer += "Plong"
+
+    if number % 7 != 0 and number % 5 != 0 and number % 3 != 0:
+        return str(number)
+
+    return answer

cb0a5fb79f69f6b86b35583021abf04285ff8bdf
Alejandro Alonso Iglesias <alejandro.aloigl@gmail.com>
2026-07-27 19:44:51 +0200

    Crear Resistor Color Duo (Exercise 35).py

---
+++ 2026/Resistor Color Duo (Exercise 35).py
@@ -0,0 +1,6 @@
+dict_colors = {"black": 0, "brown": 1, "red": 2, "orange": 3, "yellow":  4, "green": 5, "blue": 6, "violet": 7, "grey": 8, "white": 9}
+
+def value(colors):
+    answer = ""
+    answer += str(dict_colors[colors[0]]) + str(dict_colors[colors[1]])
+    return int(answer)

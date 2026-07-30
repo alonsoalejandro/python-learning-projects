c243f627ac5e7adead4890c74f8a6b4c78d6ae66
Alejandro Alonso Iglesias <alejandro.aloigl@gmail.com>
2026-07-30 17:59:47 +0200

    Crear Hamming (Exercise 42).py

---
+++ 2026/Hamming (Exercise 42).py
@@ -0,0 +1,8 @@
+def distance(strand_a, strand_b):
+    hamming_distance = 0
+    if len(strand_a) == len(strand_b):
+        for i in range(len(strand_a)):
+            if strand_a[i] != strand_b[i]:
+                hamming_distance += 1
+        return hamming_distance
+    raise ValueError("Strands must be of equal length.")
